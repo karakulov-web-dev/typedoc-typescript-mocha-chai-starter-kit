c02182b2ca25f6e4e33dc7f43fe269008aeb6450
karakulov.web.dev <karakulov.web.dev@gmail.com>
2019-03-23 03:30:54 +0700

    v1

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+/**
+ * @author karakulov.web.dev@gmail.com
+ */
+
+/**
+ * Класс App хранит точки входа в приложение
+ */
+
+class App {
+  /**
+   *Главная точка входа в приложение
+   */
+  public static main() {
+    return 0;
+  }
+}
+
+export default App;
